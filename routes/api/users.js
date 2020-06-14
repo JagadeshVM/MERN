0baaf96a6b405a5b01f20fff90cddf9c341014b7
Jagadesh.V <jaagadeshvm@gmail.com>
2020-06-14 13:01:24 +0530

    profile model and auth middleware added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -21,7 +23,6 @@ router.post(
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
-    // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -59,8 +60,17 @@ router.post(
       await user.save();
 
       //return JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 360000}, (err, token)=>{
+        if(err) throw err;
+        res.json({ token });
+      });
 
-      res.send('user registered');
+      // res.send('user registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('server error');
